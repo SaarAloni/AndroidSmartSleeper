e79c6e116896e2a03823b2ac98c43e704a7624ff
Artiom Divak <artten12380@gmail.com>
2023-03-14 14:20:15 +0200

    Add login page
    added login page and successfully logined with test db

---
+++ app/src/main/java/com/example/atry/activities/LoginActivity.java
@@ -0,0 +1,46 @@
+package com.example.atry.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.atry.R;
+
+import org.chromium.net.CronetEngine;
+import org.chromium.net.UrlRequest;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button login = findViewById(R.id.buttonLogin);
+        EditText password = findViewById(R.id.editTextPassword);
+        EditText email = findViewById(R.id.editTextEmail);
+        CronetEngine.Builder myBuilder = new CronetEngine.Builder(this);
+        CronetEngine cronetEngine = myBuilder.build();
+        Executor executor = Executors.newSingleThreadExecutor();
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                UrlRequest.Builder requestBuilder = cronetEngine.newUrlRequestBuilder(
+                        "http://192.168.1.206:5000/login?" +
+                                "email=" +  email.getText() +
+                                "&password=" + password.getText(),
+                        new MyUrlRequestCallback(), executor);
+
+                UrlRequest request = requestBuilder.build();
+                request.start();
+            }
+        });
+    }
+}
